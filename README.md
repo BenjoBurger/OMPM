6be89bc364a65542fdb79840c4eb23f4c24acbec
BenjoBurger <benjaminggr@gmail.com>
2023-12-12 17:52:18 +0800

    Added Start Command

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Handle start and help
 - Help: Drop down menu? / Functions inside
 Add people who owe me money
 - How to calculate: Each person / Split among people
-- Calculator: create keypad?
+- Calculator: Create keypad?
 - QOL: GST + SST / GST / SST
 - Splitting among people
 Check who owes me money
@@ -16,3 +16,6 @@ Add bot to chat and leave when paid:
 1. Add using telehandle?
 2. Private / group chat?
 3. How to confirm payment?
+
+TODO:
+1. How to connect the bot to a database
--- moneybot.py
+++ moneybot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import os
 
 API_KEY = os.getenv('API_KEY')
@@ -8,12 +9,26 @@ bot = telebot.TeleBot(API_KEY)
 # Handles '/start'
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-	bot.reply_to(message, "What do you want to do today?")
+	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+	btn1 = types.KeyboardButton("/add")
+	btn2 = types.KeyboardButton("/who")
+	btn3 = types.KeyboardButton("/help")
+	markup.add(btn1,btn2)
+	markup.add(btn3)
+	bot.send_message(chat_id=message.chat.id, text="What do you want to do today?", reply_markup=markup)
 
 # Handles help
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-	bot.reply_to(message, "Who owe you money?\nAdd people who owe you money")
+	bot.reply_to(message, "/add - Add people who owe you money \n/who - Who owe you money?")
+
+@bot.message_handler(commands=['add'])
+def handle_add(message):
+	bot.reply_to(message, "Who owe you money")
+
+@bot.message_handler(commands=['who'])
+def handle_who(message):
+	bot.reply_to(message, "Who owing you money")
 
 # Main Functionalities:
 # Handle start and help
@@ -27,4 +42,4 @@ def handle_help(message):
 # Check who owes me money
 # Reminder of who owe me money
 
-bot.infinity_polling()
+bot.polling()
